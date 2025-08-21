ed5bdb16faabe9b9cb14aba9566288da9f184993
Victor Hernandez <victormanu968@gmail.com>
2025-08-20 22:01:13 -0500

    add (ETL_PostgreSQL_test) python operator to execute an http request

---
+++ dags/ETL_PostgreSQL.py
@@ -0,0 +1,43 @@
+# Imports
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from datetime import datetime, timedelta
+
+# arguments for DAG
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    'start_date': datetime(2020, 1, 1)
+}
+
+# functions for tasks (it can be created outside the DAG, depending on your needs and conventions)
+def get_api():
+    # the libreries must be inside the function, because each task is a different process executed by a different worker
+    import requests
+    url = "https://jsonplaceholder.typicode.com/posts/1"
+    headers = {}
+    response = requests.request("GET", url, headers=headers)
+    # save the response in a csv file
+    with open('/tmp/sales_db_py.csv', 'wb') as f:
+        f.write(response.content)
+    
+    
+
+with DAG(
+    'ETL_PostgreSQL',
+    default_args=default_args,
+    description='ETL PostgreSQL',
+    schedule=None,
+    tags=['ETL', 'Ingenieria']
+) as dag:
+    
+    get_api_python = PythonOperator(
+        task_id='get_api_python',
+        python_callable=get_api
+    )
